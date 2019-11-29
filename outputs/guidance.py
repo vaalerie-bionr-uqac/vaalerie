3d48ea94852450da02f5121c16e51e3a2d08290b
vaalerie-bionr-uqac <vaalerie.uqac@gmail.com>
2019-11-29 15:06:31 -0500

    Project import: Modifications for transfer from Tinker board S to Rasp PI 4 and code modif. for PCA9685 PWM control. Steering and speed control is functionnal. Mapped values range from 1 to 2 ms for pulses.

--- outputs/guidance.py
+++ outputs/guidance.py
@@ -9,6 +9,7 @@ project: V.A.A.L.E.R.I.E. <vaalerie.uqac@gmail.com>
 """
 
 import Adafruit_PCA9685
+import time
 
 
 class Guidance:
@@ -34,7 +35,7 @@ class Guidance:
     def brake_is_on(self, state):
         # Open rear lights when brakes are applied
         if state:
-            self.pca.set_pwm(self.rear_light, 0, int(self.hz*4.096*10.0))
+            self.pca.set_pwm(self.rear_light, 0, 4096)
         else:
             self.pca.set_pwm(self.rear_light, 0, 0)
 
@@ -43,9 +44,9 @@ class Guidance:
         self.pca = Adafruit_PCA9685.PCA9685()
         self.pca.set_pwm_freq(self.hz)
 
-        # Initialize motor pin to 0 speed on init (1.5 ms)
-        self.pca.set_pwm(self.motor, 0, int(self.hz*4.096*1.5))
-
+        # Initialize motor pin to 0 speed on init (1.5 ms - corrected to 4096 res.)
+        self.pca.set_pwm(self.motor, 0, 594)
+        time.sleep(0.5)
         # Setup TINKERBOARD GPIO's
         """gpio.setwarnings(False)
         gpio.setmode(gpio.BOARD)
